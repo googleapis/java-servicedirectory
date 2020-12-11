4d6959979cde26c7a28401d7e642e5e1979bef93
yoshi-automation <yoshi-automation@google.com>
2020-12-10 18:17:45 -0800

    fix: Update gapic-generator-java to 0.0.7
    Committer: @miraleung
    PiperOrigin-RevId: 345476969
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Dec 3 10:07:32 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 7be2c821dd88109038c55c89f7dd48f092eeab9d
    Source-Link: https://github.com/googleapis/googleapis/commit/7be2c821dd88109038c55c89f7dd48f092eeab9d

--- proto-google-cloud-servicedirectory-v1beta1/src/main/java/com/google/cloud/servicedirectory/v1beta1/ServiceName.java
+++ proto-google-cloud-servicedirectory-v1beta1/src/main/java/com/google/cloud/servicedirectory/v1beta1/ServiceName.java
@@ -121,7 +121,7 @@ public class ServiceName implements ResourceName {
   public static List<String> toStringList(List<ServiceName> values) {
     List<String> list = new ArrayList<>(values.size());
     for (ServiceName value : values) {
-      if (Objects.isNull(value)) {
+      if (value == null) {
         list.add("");
       } else {
         list.add(value.toString());
@@ -136,20 +136,20 @@ public class ServiceName implements ResourceName {
 
   @Override
   public Map<String, String> getFieldValuesMap() {
-    if (Objects.isNull(fieldValuesMap)) {
+    if (fieldValuesMap == null) {
       synchronized (this) {
-        if (Objects.isNull(fieldValuesMap)) {
+        if (fieldValuesMap == null) {
           ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
-          if (!Objects.isNull(project)) {
+          if (project != null) {
             fieldMapBuilder.put("project", project);
           }
-          if (!Objects.isNull(location)) {
+          if (location != null) {
             fieldMapBuilder.put("location", location);
           }
-          if (!Objects.isNull(namespace)) {
+          if (namespace != null) {
             fieldMapBuilder.put("namespace", namespace);
           }
-          if (!Objects.isNull(service)) {
+          if (service != null) {
             fieldMapBuilder.put("service", service);
           }
           fieldValuesMap = fieldMapBuilder.build();
